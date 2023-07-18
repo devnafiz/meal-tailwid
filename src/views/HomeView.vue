9ab41de3a1c0b7e64cfea3db450e23979f1fd444
nafiz <nafiz.2011@yahoo.com>
2023-07-18 17:17:28 +0600

    add axios for meal api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,24 @@ import TheWelcome from '../components/TheWelcome.vue'
 import { computed } from 'vue';
 import store from '../store';
 
-const meals=computed(()=>store.state.meals)
+
+
+const letters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 </script>
 
 <template>
-   {{ meals }}
+  <div class="flex flex-col items-center ">
+
+     <input class="rounded border-2  border-gray-200 w-full justify-center" placeholder="Search for meals" type="text"/> 
+
+     <div class="flex gap-2 mt-2 justify-center">
+       <router-link :to="{name:'byLetter', params:{letter}}" v-for="letter of letters" :key="letter">
+           {{ letter }}
+
+       </router-link>
+
+     </div>
+    
+
+  </div>
 </template>
