234150fd53916c5d1ceaa7d988c8117560bbd32d
nafiz <nafiz.2011@yahoo.com>
2023-08-07 14:57:44 +0600

    add search

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@
     <div class=" bg-gray-100 h-full">
         <header class=" bg-white shadow-md h-16 flex justify-center items-stretch">
 
-            <router-link :to="{name:'home' }">Home</router-link>
-            <div>
-                <router-link to="/" class=" px-2 h-full hover:bg-slate-300">Search Meal</router-link>
-                <router-link to="/"  class=" px-2">Search Meal by Letter</router-link>
-                <router-link to="/"  class=" px-2"> Meals By Ingradients</router-link>
+            <router-link :to="{name:'home' }" class="inline-flex items-center h-full">Home</router-link>
+            <div class=" flex items-center gap-2">
+                <router-link to="/" class=" inline-flex items-center px-2 h-full hover:bg-slate-300">Search Meal</router-link>
+                <router-link to="/"  class="inline-flex items-center px-2">Search Meal by Letter</router-link>
+                <router-link to="/"  class="inline-flex items-center px-2"> Meals By Ingradients</router-link>
             </div>
 
         </header>
