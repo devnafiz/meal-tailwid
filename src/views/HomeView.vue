c234820b8a633ec587901624d31957f29650a222
nafiz <nafiz.2011@yahoo.com>
2023-08-29 11:58:54 +0600

    add meals by letter  details

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,10 +23,7 @@ onMounted( async()=>{
      <input class="rounded border-2  border-gray-200 w-full justify-center" placeholder="Search for meals" type="text"/> 
 
      <div class="flex gap-2 mt-2 justify-center">
-       <router-link :to="{name:'byLetter', params:{letter}}" v-for="letter of letters" :key="letter">
-           {{ letter }}
-
-       </router-link>
+      
 
      </div>
      <div class="flex w-100">
--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>
-   By Letter
+  <div class=" flex justify-center gap-2 mt-3">
+    <router-link :to="{name:'byLetter', params:{letter}}" v-for="letter of letters" :key="letter">
+           {{ letter }}
+
+       </router-link>
   </div>
 
 </template>
 
 <script setup>
+
+import { computed, onMounted,ref } from 'vue';
+import store from '../store';
+import axiosClient from '../axiosClient.js';
+
+
+
+const letters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const ingradients =ref([])
+
 </script>
